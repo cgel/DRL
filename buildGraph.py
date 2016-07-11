9449a746a778da94a3f1b229d0ea006bc357fdb6
cgel <carlesgelada@hotmail.com>
2016-07-11 11:13:26 +0200

    add clipped loss and add name scopes

--- buildGraph.py
+++ buildGraph.py
@@ -39,38 +39,55 @@ def createQNetwork(summaryCollection, action_num):
 
     input_state_placeholder = tf.placeholder("float",[None,84,84,4], name=summaryCollection+"/state_placeholder")
 
-    # hidden layers
-    h_conv1 = tf.nn.relu(conv2d(input_state_placeholder,W_conv1,4) + b_conv1)
-    #h_pool1 = self.max_pool_2x2(h_conv1)
+    with tf.name_scope("conv1"):
+        h_conv1 = tf.nn.relu(conv2d(input_state_placeholder,W_conv1,4) + b_conv1, name="conv1")
+        build_activation_summary(h_conv1, summaryCollection)
+        #h_pool1 = self.max_pool_2x2(h_conv1)
 
-    h_conv2 = tf.nn.relu(conv2d(h_conv1,W_conv2,2) + b_conv2)
+    with tf.name_scope("conv2"):
+        h_conv2 = tf.nn.relu(conv2d(h_conv1,W_conv2,2) + b_conv2, name="conv2")
+        build_activation_summary(h_conv2, summaryCollection)
 
-    h_conv3 = tf.nn.relu(conv2d(h_conv2,W_conv3,1) + b_conv3)
+    with tf.name_scope("conv3"):
+        h_conv3 = tf.nn.relu(conv2d(h_conv2,W_conv3,1) + b_conv3, name="conv3")
+        build_activation_summary(h_conv3, summaryCollection)
 
-    h_conv3_shape = h_conv3.get_shape().as_list()
-    h_conv3_flat = tf.reshape(h_conv3,[-1, h_conv3_shape[1] * h_conv3_shape[2] * h_conv3_shape[3]])
+    with tf.name_scope("flatten"):
+        h_conv3_shape = h_conv3.get_shape().as_list()
+        h_conv3_flat = tf.reshape(h_conv3,[-1, h_conv3_shape[1] * h_conv3_shape[2] * h_conv3_shape[3]], name="conv3_flat")
 
-    h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat,W_fc1) + b_fc1)
+    with tf.name_scope("linear1"):
+        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat,W_fc1) + b_fc1, name="linear1")
+        build_activation_summary(h_fc1, summaryCollection)
 
-    # Q Value layer
-    Q = tf.matmul(h_fc1,W_fc2) + b_fc2
-    build_activation_summary(Q, summaryCollection)
+    with tf.name_scope("Qs"):
+        Q = tf.add(tf.matmul(h_fc1,W_fc2), b_fc2, name="Qs")
+        build_activation_summary(Q, summaryCollection)
+
     paramList = [W_conv1,b_conv1,W_conv2,b_conv2,W_conv3,b_conv3,W_fc1,b_fc1,W_fc2,b_fc2]
-    #for param in paramList:
-        #build_activation_summary(param, summaryCollection)
+    for param in paramList:
+        build_activation_summary(param, summaryCollection)
 
     return input_state_placeholder, Q, paramList
 
 
 def build_train_op(DQN, Y, action, action_num):
-    action_one_hot = tf.one_hot(action, action_num, 1., 0., name='action_one_hot')
-    DQN_acted = tf.reduce_sum(DQN * action_one_hot, reduction_indices=1, name='DQN_acted')
+    with tf.name_scope("loss"):
+        action_one_hot = tf.one_hot(action, action_num, 1., 0., name='action_one_hot')
+        DQN_acted = tf.reduce_sum(DQN * action_one_hot, reduction_indices=1, name='DQN_acted')
 
-    batch_loss = tf.square(Y - DQN_acted)
-    loss = tf.reduce_mean(batch_loss)
-    tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_average_loss", loss))
-    tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_loss_0", batch_loss[0]))
-    tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_Y_0", Y[0]))
-    tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_actedDQN_0", DQN_acted[0]))
-    #tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_maxDQN_0", tf.reduce_max(DQN[0])))
-    return tf.train.RMSPropOptimizer(0.00025,0.99,0.0,1e-6).minimize(loss)
+        #cliping gradient for each example
+        delta_grad_clip = 1
+        batch_delta = Y - DQN_acted
+        batch_delta_abs = tf.abs(batch_delta)
+        batch_delta_quadratic = tf.minimum(batch_delta_abs, delta_grad_clip)
+        batch_delta_linear = (batch_delta_abs - batch_delta_quadratic)*2
+        batch_loss = batch_delta_linear + batch_delta_quadratic**2
+        #batch_loss = (Y - DQN_acted)**2
+        loss = tf.reduce_mean(batch_loss)
+        tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_average_loss", loss))
+        tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_loss_0", batch_loss[0]))
+        tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_Y_0", Y[0]))
+        tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_actedDQN_0", DQN_acted[0]))
+        #tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_maxDQN_0", tf.reduce_max(DQN[0])))
+    return tf.train.RMSPropOptimizer(0.00025,0.95,0.95,0.01).minimize(loss)
