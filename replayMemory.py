a6ec767dbf75240fc9d80236a0609af48bd40364
cgel <carlesgelada@hotmail.com>
2016-07-28 07:10:38 +0200

    solve RM int to uint bug

--- replayMemory.py
+++ replayMemory.py
@@ -6,12 +6,12 @@ class ReplayMemory:
         self.capacity = config.replay_memory_capacity
         self.batch_size = config.batch_size
         self.buff_size = config.buff_size
-        self.screens = np.empty((self.capacity, 84,84), dtype=np.int8)
-        self.actions = np.empty((self.capacity), dtype=np.int8)
+        self.screens = np.empty((self.capacity, 84,84), dtype=np.uint8)
+        self.actions = np.empty((self.capacity), dtype=np.uint8)
         self.rewards = np.empty((self.capacity), dtype=np.int8)
         self.terminals = np.empty((self.capacity), dtype=np.bool)
-        self.next_state_batch = np.empty((self.batch_size, 84, 84, 4), dtype=np.int8)
-        self.state_batch = np.empty((self.batch_size, 84, 84, 4), dtype=np.int8)
+        self.next_state_batch = np.empty((self.batch_size, 84, 84, 4), dtype=np.uint8)
+        self.state_batch = np.empty((self.batch_size, 84, 84, 4), dtype=np.uint8)
         self.current = 0
         self.step = 0
         self.filled = False
@@ -35,26 +35,28 @@ class ReplayMemory:
             state = self.screens[(index - self.buff_size+1):(index + 1), ...]
         #Slow list read
         else:
-            indexes = [(index - i) % self.step for i in reversed(range(self.buff_size))]
+            indexes = [(index - i) % self.capacity for i in reversed(range(self.buff_size))]
             state = self.screens[indexes, ...]
         # different screens should be in the 3rd dim as channels
         return np.transpose(state, [1,2,0])
 
     def sample_transition_batch(self):
-        assert self.current >= self.batch_size, "There is not enough to sample. Call add bathc_size times"
+        if self.filled == False:
+          assert self.current >= self.batch_size, "There is not enough to sample. Call add bathc_size times"
         indexes = []
         while len(indexes) != self.batch_size:
             # index, is the index of state, and index + 1 of next_state
             if self.filled:
                 index = random.randint(0, self.capacity - 2) # -2 because index +1 will be used for next state
+                # if index is in the space we are currently writing
                 if index >= self.current and index - self.buff_size < self.current:
                     continue
             else:
                 # can't start from 0 because get_state would loop back to the end -- wich is uninitialized.
                 # index +1 can be terminal
-                index = random.randint(self.buff_size -1, self.current - 2)
+                index = random.randint(self.buff_size -1, self.current -2)
 
-            if self.terminals[(index - self.buff_size):index].any():
+            if self.terminals[(index - self.buff_size -1):index].any():
                 continue
             self.state_batch[len(indexes)] = self.get_state(index)
             self.next_state_batch[len(indexes)] = self.get_state(index + 1)
@@ -62,4 +64,4 @@ class ReplayMemory:
         action_batch = self.actions[indexes]
         reward_batch = self.rewards[indexes]
         terminal_batch = self.terminals[indexes]
-        return self.state_batch, action_batch, reward_batch, self.next_state_batch, terminal_batch
+        return self.state_batch, action_batch, reward_batch, self.next_state_batch, terminal_batch, indexes
