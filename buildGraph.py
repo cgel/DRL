b753207ee75c865185a7268970730b3c5d940da9
cgel <carlesgelada@hotmail.com>
2016-09-13 03:21:16 +0200

    use a thread to queue batches. Big perf improvements ~2X

--- buildGraph.py
+++ buildGraph.py
@@ -14,10 +14,10 @@ def conv2d(x, W, stride, name):
 def xavier_std(in_size, out_size):
     return np.sqrt(2./(in_size + out_size))
 
-def createQNetwork(summaryCollection, action_num, action_placeholder):
+#createQNetwork("DQN_summaries", action_num, input_state, action)
+def createQNetwork(input_state, action, action_num, summaryCollection):
     conv_layer_counter = [0]
     linear_layer_counter = [0]
-    conditional_linear_layer_counter = [0]
     weight_list = []
 
     def add_conv_layer(nn_head, channels, kernel_size, stride):
@@ -54,9 +54,7 @@ def createQNetwork(summaryCollection, action_num, action_placeholder):
         build_activation_summary(new_head, _summaryCollection)
         return new_head
 
-    input_state_placeholder = tf.placeholder("float",[None,84,84,4], name=summaryCollection+"/state_placeholder")
-    # this should be: input_state_placeholder = tf.placeholder("float",[None,84,84,4], name="state_placeholder")
-    normalized = input_state_placeholder / 256.
+    normalized = input_state/ 256.
     tf.add_to_collection(summaryCollection, tf.histogram_summary("normalized_input", normalized))
 
     nn_head = add_conv_layer(normalized, channels=32, kernel_size=8, stride=4)
@@ -64,10 +62,10 @@ def createQNetwork(summaryCollection, action_num, action_placeholder):
     nn_head = add_conv_layer(nn_head, channels=64, kernel_size=3, stride=1)
 
     h_conv3_shape = nn_head.get_shape().as_list()
-    state = tf.reshape(nn_head, [-1, h_conv3_shape[1] * h_conv3_shape[2] * h_conv3_shape[3]], name="conv3_flat")
-    state_shape = state.get_shape().as_list()
+    hidden_state = tf.reshape(nn_head, [-1, h_conv3_shape[1] * h_conv3_shape[2] * h_conv3_shape[3]], name="conv3_flat")
+    hidden_state_shape = hidden_state.get_shape().as_list()
 
-    def state_to_Q(nn_head, _name, _summaryCollection):
+    def hidden_state_to_Q(nn_head, _name, _summaryCollection):
         nn_head = add_linear_layer(nn_head, size=512, _summaryCollection=_summaryCollection, layer_name="final_linear_"+_name, weight_name="final_linear_Q_W")
         # the last layer is linear without a relu
         nn_head_size = nn_head.get_shape().as_list()[1]
@@ -78,19 +76,17 @@ def createQNetwork(summaryCollection, action_num, action_placeholder):
         return Q
 
     # the functions state -> Q, and future_state -> future_Q are the same and share parameters
-    Q = state_to_Q(state, "Q", summaryCollection)
+    Q = hidden_state_to_Q(hidden_state, "Q", summaryCollection)
 
-    # lets take this layer as the state for next state predictions
-    #append one hot condition to head_future
-    action_one_hot = tf.one_hot(action_placeholder, action_num, 1., 0., name='action_one_hot')
-    nn_head_future = tf.concat(1, [action_one_hot, state], name="one_hot_concat_state")
-    nn_head_future = add_linear_layer(nn_head_future, size=512, _summaryCollection=summaryCollection+"_prediction", layer_name="future_state_linear_1")
-    future_state = add_linear_layer(nn_head_future, size=state_shape[1], _summaryCollection=summaryCollection+"_prediction", layer_name="future_state_linear_2")
+    action_one_hot = tf.one_hot(action, action_num, 1., 0., name='action_one_hot')
+    predicted_next_hidden_state = tf.concat(1, [action_one_hot, hidden_state], name="one_hot_concat_state")
+    predicted_next_hidden_state = add_linear_layer(predicted_next_hidden_state, size=512, _summaryCollection=summaryCollection+"_prediction", layer_name="predicted_next_hidden_state_linear_1")
+    predicted_next_hidden_state = add_linear_layer(predicted_next_hidden_state, size=hidden_state_shape[1], _summaryCollection=summaryCollection+"_prediction", layer_name="predicted_next_hidden_state_linear_2")
 
     tf.get_variable_scope().reuse_variables()
-    future_Q = state_to_Q(future_state, "future_Q", summaryCollection+"_prediction")
+    predicted_next_Q = hidden_state_to_Q(predicted_next_hidden_state, "future_Q", summaryCollection+"_prediction")
 
-    return input_state_placeholder, Q, future_Q, weight_list
+    return Q, predicted_next_Q, weight_list
 
 def clipped_l2(y, y_t):
     with tf.name_scope("clipped_l2"):
@@ -102,29 +98,29 @@ def clipped_l2(y, y_t):
         batch = batch_delta_linear + batch_delta_quadratic**2
     return batch
 
-def build_train_op(DQN, Y, action, future_Q, next_max_Q, action_num, lr, alpha=1):
+#build_train_op(Q, Y, action, predicted_next_Q, next_Y, action_num, learning_rate)
+def build_train_op(Q, Y, action, predicted_next_Q, next_Y, action_num, lr, alpha=1):
     with tf.name_scope("loss"):
         action_one_hot = tf.one_hot(action, action_num, 1., 0., name='action_one_hot')
-        DQN_acted = tf.reduce_sum(DQN * action_one_hot, reduction_indices=1, name='DQN_acted')
-        max_future_Q = tf.reduce_max(future_Q, 1)
+        DQN_acted = tf.reduce_sum(Q * action_one_hot, reduction_indices=1, name='DQN_acted')
 
         loss = tf.reduce_mean(clipped_l2(Y, DQN_acted))
-        future_loss = tf.reduce_sum(clipped_l2(max_future_Q, next_max_Q), name="future_loss")
+        future_loss = tf.reduce_sum(clipped_l2(predicted_next_Q, next_Y), name="future_loss")
         combined_loss = loss + alpha*future_loss
 
+        max_predicted_next_Q = tf.reduce_max(predicted_next_Q, 1)
+        next_max_Q = tf.reduce_max(next_Y, 1)
         tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_average_loss", loss))
         tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_average_future_loss", future_loss))
         tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_average_combined_loss", combined_loss))
         tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_Y_0", Y[0]))
-        tf.add_to_collection("DQN_summaries", tf.scalar_summary("max_future_Q_0", max_future_Q[0]))
+        tf.add_to_collection("DQN_summaries", tf.scalar_summary("max_predicted_next_Q_0", max_predicted_next_Q[0]))
         tf.add_to_collection("DQN_summaries", tf.scalar_summary("next_max_Q_0", next_max_Q[0]))
         tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_actedDQN_0", DQN_acted[0]))
         #tf.add_to_collection("DQN_summaries", tf.scalar_summary("rm_maxDQN_0", tf.reduce_max(DQN[0])))
 
     opti = tf.train.RMSPropOptimizer(lr,0.95,0.95,0.01)
-    #opti = tf.train.GradientDescentOptimizer(lr)
     grads = opti.compute_gradients(combined_loss)
-
     train_op = opti.apply_gradients(grads) # have to pass global_step ?????
 
     for grad, var in grads:
