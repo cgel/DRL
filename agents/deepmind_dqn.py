4994ec07e74fffe1eacb21f6e38ab357554de116
cgel <carlesgelada@hotmail.com>
2016-11-24 03:37:11 +0100

    beaufity

--- agents/deepmind_dqn.py
+++ agents/deepmind_dqn.py
@@ -6,7 +6,9 @@ import threading
 from replayMemory import ReplayMemory
 import commonOps
 
+
 class Agent:
+
     def __init__(self, config, session, summary_writter):
         # build the net
         self.config = config
@@ -31,15 +33,22 @@ class Agent:
             with tf.variable_scope("Q"):
                 self.Q = commonOps.deepmind_Q(self.state, config, "Normal")
             with tf.variable_scope("QT"):
-                self.QT = commonOps.deepmind_Q(self.stateT_ph, config, "Target")
-            self.sync_QT_op = [W_pair[0].assign(W_pair[1]) for W_pair in zip(tf.get_collection("Normal_weights"), tf.get_collection("Target_weights"))]
-            self.train_op = commonOps.build_train_op(self.Q, self.Y, self.action, config, "Normal")
+                self.QT = commonOps.deepmind_Q(
+                    self.stateT_ph, config, "Target")
+            self.sync_QT_op = [
+                W_pair[0].assign(
+                    W_pair[1]) for W_pair in zip(
+                    tf.get_collection("Normal_weights"),
+                    tf.get_collection("Target_weights"))]
+            self.train_op = commonOps.build_train_op(
+                self.Q, self.Y, self.action, config, "Normal")
             self.Q_summary_op = tf.merge_summary(
                 tf.get_collection("Normal_summaries"))
             self.QT_summary_op = tf.merge_summary(
                 tf.get_collection("Target_summaries"))
         self.reset_game()
-        self.enqueue_from_RM_thread = threading.Thread(target=self.enqueue_from_RM)
+        self.enqueue_from_RM_thread = threading.Thread(
+            target=self.enqueue_from_RM)
         self.enqueue_from_RM_thread.daemon = True
         self.stop_enqueuing = threading.Event()
         self.timeout_option = tf.RunOptions(timeout_in_ms=5000)
@@ -47,7 +56,9 @@ class Agent:
     def step(self, x, r):
         if self.training:
             if not self.episode_begining:
-                self.RM.add(self.game_state[:,:,:,-1], self.game_action, self.game_reward, False)
+                self.RM.add(
+                    self.game_state[
+                        :, :, :, -1], self.game_action, self.game_reward, False)
             else:
                 self.episode_begining = False
             self.game_action = self.e_greedy_action(self.epsilon())
@@ -59,10 +70,13 @@ class Agent:
             self.observe(x, r)
         return self.game_action
 
-    # Add the transition to RM and reset the internal state for the next episode
+    # Add the transition to RM and reset the internal state for the next
+    # episode
     def done(self):
         if self.training:
-            self.RM.add(self.game_state[:,:,:,-1], self.game_action, self.game_reward, True)
+            self.RM.add(
+                self.game_state[
+                    :, :, :, -1], self.game_action, self.game_reward, True)
         self.reset_game()
 
     def observe(self, x, r):
@@ -80,7 +94,8 @@ class Agent:
             if self.config.logging and self.step_count % self.config.save_summary_rate == 0:
                 _, Q_summary_str = self.sess.run(
                     [self.train_op, self.Q_summary_op], options=self.timeout_option)
-                self.summary_writter.add_summary(Q_summary_str, self.step_count)
+                self.summary_writter.add_summary(
+                    Q_summary_str, self.step_count)
             else:
                 _ = self.sess.run(self.train_op, options=self.timeout_option)
             if self.step_count % self.config.sync_rate == 0:
@@ -92,17 +107,25 @@ class Agent:
             state_batch, action_batch, reward_batch, next_state_batch, terminal_batch, _ = self.RM.sample_transition_batch()
             if self.config.logging and self.step_count % self.config.save_summary_rate == 0:
                 QT_np, QT_summary_str = self.sess.run([self.QT, self.QT_summary_op], feed_dict={
-                                                   self.stateT_ph:next_state_batch}, options=self.timeout_option)
-                self.summary_writter.add_summary(QT_summary_str, self.step_count)
+                    self.stateT_ph: next_state_batch}, options=self.timeout_option)
+                self.summary_writter.add_summary(
+                    QT_summary_str, self.step_count)
             else:
                 QT_np = self.sess.run(
-                    self.QT, feed_dict={self.stateT_ph:next_state_batch}, options=self.timeout_option)
+                    self.QT,
+                    feed_dict={
+                        self.stateT_ph: next_state_batch},
+                    options=self.timeout_option)
 
             # simplify with np
             QT_max_action = np.max(QT_np, 1)
-            Y = reward_batch + self.config.gamma*QT_max_action*(1-terminal_batch)
+            Y = reward_batch + self.config.gamma * \
+                QT_max_action * (1 - terminal_batch)
 
-            feed_dict = {self.state_ph: state_batch, self.action_ph: action_batch, self.Y_ph: Y}
+            feed_dict = {
+                self.state_ph: state_batch,
+                self.action_ph: action_batch,
+                self.Y_ph: Y}
             self.sess.run(self.enqueue_op, feed_dict=feed_dict)
         print("Closing enqueue thread")
 
@@ -110,7 +133,10 @@ class Agent:
         if np.random.uniform() < epsilon:
             action = random.randint(0, self.config.action_num - 1)
         else:
-            action = np.argmax(self.sess.run(self.Q, feed_dict={self.state: self.game_state})[0])
+            action = np.argmax(
+                self.sess.run(
+                    self.Q, feed_dict={
+                        self.state: self.game_state})[0])
         return action
 
     def testing(self, t=True):
@@ -118,11 +144,13 @@ class Agent:
 
     def reset_game(self):
         self.episode_begining = True
-        self.game_state = np.zeros((1, 84, 84, self.config.buff_size), dtype=np.uint8)
+        self.game_state = np.zeros(
+            (1, 84, 84, self.config.buff_size), dtype=np.uint8)
 
     def epsilon(self):
         if self.step_count < self.config.exploration_steps:
-            return self.config.initial_epsilon - ((self.config.initial_epsilon - self.config.final_epsilon) / self.config.exploration_steps) * self.step_count
+            return self.config.initial_epsilon - \
+                ((self.config.initial_epsilon - self.config.final_epsilon) / self.config.exploration_steps) * self.step_count
         else:
             return self.config.final_epsilon
 
--- train.py
+++ train.py
@@ -10,7 +10,7 @@ parser.add_argument("-replay_memory_capacity", type=int, default=1000000)
 parser.add_argument("-steps_before_training", type=int, default=12500)
 parser.add_argument("-exploration_steps", type=int, default=250000)
 parser.add_argument("-sync_rate", type=int, default=2500)
-parser.add_argument("-save_summary_rate", type=int, default=1000)
+parser.add_argument("-save_summary_rate", type=int, default=10000)
 parser.add_argument("-device", default="/gpu:0")
 parser.add_argument("-gamma", type=float, default=0.99)
 parser.add_argument("-learning_rate", type=float, default=0.00025)
@@ -30,7 +30,8 @@ config.log_percent_rate = 1000
 config.test_run_num = 20
 config.logging = config.logging not in ["0", "false", "False"]
 print("Logging: " + str(config.logging))
-if config.transition_function not in ["oh_concat", "expanded_concat", "conditional"]:
+if config.transition_function not in [
+        "oh_concat", "expanded_concat", "conditional"]:
     raise Exception("Not valid transition function")
 tf.logging.set_verbosity(tf.logging.ERROR)
 
@@ -54,14 +55,15 @@ if config.logging:
     run_name = str(max(int_folders + [0]) + 1)
     log_path = "log/" + run_name + "/"
     checkpoint_path = log_path + "checkpoint/"
-    print("Starting run: "+str(run_name))
-    summary_writter_ = tf.train.SummaryWriter(log_path, sess.graph, flush_secs=20)
+    print("Starting run: " + str(run_name))
+    summary_writter_ = tf.train.SummaryWriter(
+        log_path, sess.graph, flush_secs=20)
     summary_writter = summary_writter_
     os.makedirs(checkpoint_path)
-    config_log_file = open(log_path + "config.txt",'w+')
+    config_log_file = open(log_path + "config.txt", 'w+')
     config_vars_dict = vars(config)
     for var in config_vars_dict:
-        config_log_file.write(var+": "+ str(config_vars_dict[var]) + "\n")
+        config_log_file.write(var + ": " + str(config_vars_dict[var]) + "\n")
     config_log_file.close()
 
 agent = Agent(config, sess, summary_writter)
@@ -73,6 +75,7 @@ if config.load_checkpoint != "":
     saver.restore(sess, ckpt_file)
 else:
     sess.run(tf.initialize_all_variables())
+
 
 def test_run(n):
     agent.testing(True)
@@ -99,31 +102,48 @@ for episode in range(config.num_episodes):
     agent.done()
     ep_duration = time.time() - ep_begin_t
     # online Summary
-    if config.logging and episode % config.log_online_summary_rate == 0 or config.num_episodes == episode:
-        episode_online_summary = tf.Summary(value=[tf.Summary.Value(tag="online/epsilon", simple_value=agent.epsilon()),
-                                                   tf.Summary.Value(
-                                                       tag="online/score", simple_value=score),
-                                                   tf.Summary.Value(
-                                                       tag="online/global_step", simple_value=agent.step_count),
-                                                   tf.Summary.Value(tag="online/ep_duration_seconds", simple_value=ep_duration)])
+    last_episode = config.num_episodes == episode
+    if config.logging and episode % config.log_online_summary_rate == 0 or last_episode:
+        episode_online_summary = tf.Summary(
+            value=[
+                tf.Summary.Value(
+                    tag="online/epsilon",
+                    simple_value=agent.epsilon()),
+                tf.Summary.Value(
+                    tag="online/score",
+                    simple_value=score),
+                tf.Summary.Value(
+                    tag="online/global_step",
+                    simple_value=agent.step_count),
+                tf.Summary.Value(
+                    tag="online/ep_duration_seconds",
+                    simple_value=ep_duration)])
         summary_writter.add_summary(episode_online_summary, episode)
     # log percent
-    if config.logging and episode % config.log_percent_rate == 0 and episode != 0 or config.num_episodes == episode:
+    if config.logging and episode % config.log_percent_rate == 0 and episode != 0 or last_episode:
         percent = float(episode) / config.num_episodes * 100
         print("%i%% -- epsilon:%.2f" % (percent, agent.epsilon()))
     # save
-    if config.logging and episode % config.save_rate == 0 and episode != 0 or config.num_episodes == episode:
+    if config.logging and episode % config.save_rate == 0 and episode != 0 or last_episode:
         print("saving checkpoint at episode " + str(episode))
-        saver.save(sess, checkpoint_path+"run-"+run_name+"_episode", episode)
+        saver.save(sess, checkpoint_path + "run-" +
+                   run_name + "_episode", episode)
     # performance summary
-    if config.logging and episode % config.log_perf_summary_rate == 0 or config.num_episodes == episode:
+    if config.logging and episode % config.log_perf_summary_rate == 0 or last_episode:
         score_list = test_run(n=config.test_run_num)
-        performance_summary = tf.Summary(value=[tf.Summary.Value(tag="test_score/average", simple_value=sum(score_list) / len(score_list)),
-                                                tf.Summary.Value(
-                                                    tag="test_score/max", simple_value=max(score_list)),
-                                                tf.Summary.Value(
-                                                    tag="test_score/min", simple_value=min(score_list)),
-                                                ])
+        performance_summary = tf.Summary(
+            value=[
+                tf.Summary.Value(
+                    tag="test_score/average",
+                    simple_value=sum(score_list) /
+                    len(score_list)),
+                tf.Summary.Value(
+                    tag="test_score/max",
+                    simple_value=max(score_list)),
+                tf.Summary.Value(
+                    tag="test_score/min",
+                    simple_value=min(score_list)),
+            ])
         summary_writter.add_summary(performance_summary, agent.step_count)
 
 # Write summary about the run.
