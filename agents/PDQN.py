solve gradient bug with PDQN

agents/PDQN.py
@@ -11,7 +11,7 @@ class Agent(BaseAgent):
 
     def __init__(self, config, session):
         BaseAgent.__init__(self, config, session)
-        self.action_modes = {"e_greedy":self.e_greedy_action, "pan_e_greedy":self.plan_e_greedy_action}
+        self.action_modes = {"e_greedy":self.e_greedy_action, "plan_e_greedy":self.plan_e_greedy_action}
         self.default_action_mode = "e_greedy"
         self.action_mode = self.default_action_mode
         # build the net
@@ -78,12 +78,12 @@ class Agent(BaseAgent):
         else:
             #should be optimized
             # instead of dequeuing feed the game state
-            feed_dict={self.state_queue:self.game_state, self.action_queue:[0]}
-            predicted_Rs, predicted_next_Q_0 = self.sess.run([self.predicted_r, self.predicted_next_Q], feed_dict)
+            feed_dict={self.state_ph:self.game_state, self.action_ph:[0]} # action 0
+            predicted_Rs, predicted_next_Q_0 = self.sess.run([self.predicted_reward, self.predicted_next_Q], feed_dict)
             predicted_next_Qs = np.zeros(self.config.action_num)
             predicted_next_Qs[0] = np.max(predicted_next_Q_0)
             for a in range(1, self.config.action_num):
-                feed_dict={self.state_queue:self.game_state, self.action_queue:[a]}
+                feed_dict={self.state_ph:self.game_state, self.action_ph:[a]}
                 predicted_next_Qs[a] == np.max(self.sess.run(self.predicted_next_Q, feed_dict))
             action = np.argmax(predicted_Rs + self.config.gamma * predicted_next_Qs)
         return action

agents/commonOps.py
@@ -129,6 +129,7 @@ def dqn_train_op(Q, QT, action, reward, terminal, config, Collection):
     return train_op
 
 def pdqn_train_op(Q, predicted_reward, predicted_next_Q, QT, reward, action, terminal, config, Collection):
+    QT = tf.stop_gradient(QT)
     with tf.name_scope("loss"):
         # could be done more efficiently with gather_nd or transpose + gather
         action_one_hot = tf.one_hot(
@@ -141,7 +142,6 @@ def pdqn_train_op(Q, predicted_reward, predicted_next_Q, QT, reward, action, ter
         QT_max_action = tf.reduce_max(QT, 1)
         Y = reward + config.gamma * \
             QT_max_action * (1 - terminal)
-        Y = tf.stop_gradient(Y)
 
         Q_loss = tf.reduce_sum(
             clipped_l2(Y, Q_action), name="Q_loss")