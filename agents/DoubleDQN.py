c1379e97c48e77328f7a5567ad5b0422ab4cd67c
cgel <carlesgelada@hotmail.com>
2017-02-04 01:25:40 +0100

    DoubleDQN had a bug in the target rule

--- agents/DoubleDQN.py
+++ agents/DoubleDQN.py
@@ -11,7 +11,7 @@ class DoubleDQN(DQN):
             self.Q = self.Q_network(self.state_ph, "Normal")
             scope.reuse_variables()
             # the network with online weights used to select the actions of the target network
-            self.DoubleQ = self.Q_network(self.stateT_ph, "Target")
+            self.DoubleQT = self.Q_network(self.stateT_ph, "Target")
 
         with tf.variable_scope("QT"):
             self.QT = self.Q_network(
@@ -22,10 +22,12 @@ class DoubleDQN(DQN):
                 "main/next_Q_0", tf.reduce_max(self.QT, 1)[0], collections=["Target_summaries"])
 
     def Q_target(self):
-        target_action = tf.argmax(self.DoubleQ, axis=1)
+        target_action = tf.argmax(self.DoubleQT, axis=1)
         target_action_one_hot = tf.one_hot(
             target_action, self.config.action_num, 1., 0., name='target_action_one_hot')
+
         DoubleQT_acted = tf.reduce_sum(
-            self.Q * target_action_one_hot, reduction_indices=1, name='QT_acted')
-        return self.reward_ph + self.config.gamma * \
+            self.QT * target_action_one_hot, reduction_indices=1, name='DoubleQT')
+        Y =  self.reward_ph + self.config.gamma * \
             DoubleQT_acted * (1 - self.terminal_ph)
+        return Y
